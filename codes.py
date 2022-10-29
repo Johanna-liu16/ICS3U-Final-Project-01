677d2389c264779afad3b4876d7d283dd9320f74
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-10-29 19:25:04 -0400

    fix

--- codes.py
+++ codes.py
@@ -52,7 +52,7 @@ def game_scene():
         if keys & ugame.K_RIGHT:
             ship.move(ship.x + 1, ship.y)
         if keys & ugame.K_LEFT:
-            ship.move(ship.x - 1. ship.y)
+            ship.move(ship.x - 1, ship.y)
         if keys & ugame.K_UP:
             ship.move(ship.x, ship.y - 1)
         if keys & ugame.K_DOWN:
