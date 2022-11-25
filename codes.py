cfea4e4f591c94299cf4cc8964abe8e96ee1899a
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-11-24 21:25:45 -0500

    add

--- codes.py
+++ codes.py
@@ -9,6 +9,50 @@ import stage
 import ugame
 
 
+def menu_scene():
+    # this function is the main game game scene
+
+    # image banks for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # add text objects
+    text = []
+    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1.move(20,10)
+    text1.text("MT Game Studios")
+    text.append(text1)
+
+    text2 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text2.move(40, 110)
+    text2.text("PRESS START")
+    text.append(text2)
+
+    # set the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+
+    # create a stage for the background to show up on
+    #  and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites, items show up in order
+    game.layers = text + [background]
+    # render the background and initial lcation of sprite list
+    # most likely you will only render the background once per scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        # A button to fire
+        if keys & ugame.K_START:
+            print("Start")
+
+        # update game logic
+        # redraw Sprites
+        game.tick()  # wait until refresh rate finishes
+
 def game_scene():
     # this function is the main game game scene
 
@@ -102,6 +146,5 @@ def game_scene():
         game.render_sprites([ship] + [alien])
         game.tick()  # wait until refresh rate finishes
 
-
 if __name__ == "__main__":
-    game_scene()
+    menu_scene()
